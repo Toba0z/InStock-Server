6ffa6c4b36c5919b2a4586d987d3cd154514c0da
DanySA22 <winache22@gmail.com>
2024-03-11 18:55:28 -0700

    Start to add the route files

---
+++ routes/warehouseRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const { addListener } = require('nodemon')
+const router = express.Router()
+const warehouse = require('./routes')
+
+
+//Tentative endpoint denomination
+
+//get warehouse list
+
+//get inventory items list
+
+
+//get one warehouse information
+
+//get one inventory item information
+
+
+//get all the list inventory for one warehouse
+
+
+//post create a new warehouse
+
+
+//post create a new item
+
+
+//put update a warehouse
+
+//put update an inventory item
+
+// delete a warehouse
+
+//delete an inventory item
